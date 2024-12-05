42529a21f949f1269567981940842ec731a8d62f
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2024-12-05 17:18:48 -0500

    add all files, add PART 1: add documentation to sqlForPartialUpdate in sql.js

---
+++ helpers/sql.js
@@ -0,0 +1,27 @@
+const { BadRequestError } = require("../expressError");
+
+// PART 1: add documentation to sqlForPartialUpdate
+
+// This generates an SQL query fragment for a partial update of a database record.
+// uses the UPDATE statement in sql to update the database
+// uses parameters "dataToUpdate" as an object representing the fields to update and their new values
+// i.e {firstName: "Sean", age: 30}
+// uses parameter "jsToSql" as a mapping object that translates JS-style field names into SQL column names
+// i.e  { firstName: "first_name", lastName: "last_name" }
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
