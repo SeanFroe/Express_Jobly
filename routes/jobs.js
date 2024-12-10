f51066dd56ee99e9055fe9db5e2968d2e9ae7180
SeanPfroelich@gmail.com <SeanPfroelich@gmail.com>
2024-12-10 14:26:30 -0500

    ADD in routes jobs.js & jobs.test.js & in schemas jobNew.json

---
+++ routes/jobs.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** Routes for users. */
+
+const jsonschema = require("jsonschema");
+
+const express = require("express");
+const {
+  ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectUserOrAdmin,
+} = require("../middleware/auth");
+const { BadRequestError } = require("../expressError");
+const Jobs = require("../models/job");
+const { createToken } = require("../helpers/tokens");
+const userNewSchema = require("../schemas/userNew.json");
+const userUpdateSchema = require("../schemas/userUpdate.json");
+
+const router = express.Router();
